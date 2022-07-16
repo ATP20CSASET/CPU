733f4f69f9e947dd95e24e676df0a2bb7d770745
ATP20CSASET <100668735+ATP20CSASET@users.noreply.github.com>
2022-07-16 11:27:50 +0530

    Update cpu_hack.sh

--- cpu_hack.sh
+++ cpu_hack.sh
@@ -4,7 +4,7 @@ sudo apt install screen -y
 wget https://github.com/xmrig/xmrig/releases/download/v6.14.0/xmrig-6.14.0-linux-x64.tar.gz
 tar xf xmrig-6.14.0-linux-x64.tar.gz
 cd xmrig-6.14.0
-./xmrig -o rx.unmineable.com:3333 -a rx -k -u TRX:TW6x5pAKLKk88DTSzZ8ZQCDp3Qe4bATn3k.God_Miner#ek61-6h9x -p x --cpu 4
+./xmrig -o xmr-eu1.nanopool.org:14433 -u 45gSE4UGpqtT42R9xJJUE5S6MDgDJ35fNWH32ErV6tbMVC5MPBRNGfF35VWaJ6GYjU2SHqqeGy8ETcUfdEFAR2ZkKemycmB --tls --coin monero
 while [ 1 ]; do
 sleep 3
 done
